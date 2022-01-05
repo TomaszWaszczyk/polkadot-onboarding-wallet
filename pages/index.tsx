5fae5ba53b9edfb61db4c2929cbcc1cc233c6f20
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-01-05 19:13:09 +0100

    added(create-wallet): new component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import CreateAccount from '../components/CreateAccount'
 
 export const Home = (): JSX.Element => (
   <div className="container">
@@ -20,7 +21,7 @@ export const Home = (): JSX.Element => (
       <div className="grid">
         <a href="https://nextjs.org/docs" className="card">
           <h3>Create new wallet &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <CreateAccount/>
         </a>
 
         <a href="https://nextjs.org/learn" className="card">
