13ce0f08325c1fdd1f65c3a318d1f4fe2b8f6d53
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-01-05 01:10:14 +0100

    added(init-view): first grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,53 +4,30 @@ import Image from 'next/image'
 export const Home = (): JSX.Element => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>Polkadot Web Wallet Tutorial</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">web wallet for Polkadot</a>
+        Build a web wallet focused on user-onboarding for <a href="https://polkadot.network/"> Polkadot</a>
       </h1>
 
       <p className="description">
-        Get started by editing <code>pages/index.tsx</code>
+        A simple, non-custodial crypto wallet for managing Polkadot digital assets.
       </p>
-
-      <button
-        onClick={() => {
-          window.alert('With typescript and Jest')
-        }}
-      >
-        Test Button
-      </button>
 
       <div className="grid">
         <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
+          <h3>Create new wallet &rarr;</h3>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
 
         <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
+          <h3>Import a wallet &rarr;</h3>
           <p>Learn about Next.js in an interactive course with quizzes!</p>
         </a>
 
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
       </div>
     </main>
 
@@ -133,15 +110,6 @@ export const Home = (): JSX.Element => (
       .description {
         line-height: 1.5;
         font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
       }
 
       .grid {
